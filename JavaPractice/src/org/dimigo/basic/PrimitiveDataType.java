b5dd76662ba558556be47e4c741856a2e1417087
최은선 <@BB>
2015-03-16 09:35:53 +0900

    첫번째 실습과제 수정

--- JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -11,25 +11,27 @@ package org.dimigo.basic;
 public class PrimitiveDataType {
 
 	public static void main(String[] args) {
-			String name = "아이유";
-			boolean ismale = false;
-			int age = 23;
-			double height = 161.8;
-			float weight = 44.3f;
-			char bloodtype = 'A';
-			
-			System.out.println("<< 아이유 프로필 >>");
-			System.out.printf("이름 : %s\n", name);
-			System.out.printf("성별 : ");
-			if (ismale == true){
-				System.out.println("남자");
-			}else{
-				System.out.println("여자");
-			}
-			System.out.printf("나이 : %d\n", age);
-			System.out.printf("키 : %.1f\n", height);
-			System.out.printf("몸무게 : %.1f\n", weight);
-			System.out.printf("혈액형 : %c\n", bloodtype);
+		//코드 수정
+		
+		String name = "아이유";
+		boolean ismale = false;
+		int age = 23;
+		double height = 161.8;
+		float weight = 44.3f;
+		char bloodtype = 'A';
+		
+		System.out.println("<< 아이유 프로필 >>");
+		System.out.printf("이름 : %s\n", name);
+		System.out.printf("성별 : ");
+		if (ismale == true){
+			System.out.println("남자");
+		}else{
+			System.out.println("여자");
+		}
+		System.out.printf("나이 : %d\n", age);
+		System.out.printf("키 : %.1f\n", height);
+		System.out.printf("몸무게 : %.1f\n", weight);
+		System.out.printf("혈액형 : %c\n", bloodtype);
 	}
 
 }
