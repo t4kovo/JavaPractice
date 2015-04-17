8b8e8ed0ae56c4b82bcab3e1d43273d2a367293c
최은선 <@BB>
2015-04-17 18:26:09 +0900

    빠순냄새나는 논술형평가 클래스 템플릿

---
+++ JavaPractice/src/org/dimigo/basic/ZICO.java
@@ -0,0 +1,113 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class ZICO {
+
+    /**
+     * 
+     */
+    public ZICO() {
+    }
+
+    /**
+     * 지코 랩네임
+     */
+    private String rapName;
+
+    /**
+     * 지코 이름
+     */
+    private String name;
+
+    /**
+     * 지코 성별
+     */
+    private boolean gender;
+
+    /**
+     * 지코 나이
+     */
+    private int age;
+
+    /**
+     * 지코 혈액형
+     */
+    private String bloodType;
+
+    /**
+     * 지코 소속그룹
+     */
+    private String group;
+
+    /**
+     * 지코 소속그룹 내 포지션
+     */
+    private String position;
+
+    /**
+     * 인터뷰, 사담, 회의 등 말하기
+     * @return
+     */
+    public void talking() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 공연, 녹음 등 랩하기
+     * @return
+     */
+    public void rapping() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 다른 가수 곡에 피쳐링하기
+     * @return
+     */
+    public void featuring() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 연습, 공연, 기분 좋음을 표현하기 등 춤추기
+     * @return
+     */
+    public void dancing() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 작곡하기
+     * @return
+     */
+    public void composing() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 팬들에게 잘생김 뽐내기
+     * @return
+     */
+    public void handsoming() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 같은 그룹 멤버들에게 리더로서의 리더쉽을 발휘하여 그룹의 능률 올리기, 팬들의 행동에 칭찬, 비판을 하며 팬들의 사상을 올바르게 키워주기
+     * @return
+     */
+    public void leadership() {
+        // TODO implement here
+        return null;
+    }
+
+}
